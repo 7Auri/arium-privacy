2c4198cf1d967a62d84a64abd585c133bfe35418
Büşra Yeşilalioğlu <yesilalioglubusra@gmail.com>
2025-11-21 23:00:58 +0300

    Initial Commit

---
+++ Arium/AriumApp.swift
@@ -0,0 +1,17 @@
+//
+//  AriumApp.swift
+//  Arium
+//
+//  Created by Zorbey on 21.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct AriumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AriumTests/AriumTests.swift
@@ -0,0 +1,17 @@
+//
+//  AriumTests.swift
+//  AriumTests
+//
+//  Created by Zorbey on 21.11.2025.
+//
+
+import Testing
+@testable import Arium
+
+struct AriumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
